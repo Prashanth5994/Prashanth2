b0c25ef43615cfbe47435b49dd58d592f7c577af
Prashanth5994 <prasanth.ch1604@gmail.com>
2024-07-18 16:05:54 +0530

    feature202 changes in Application.java

--- Applicstion.java
+++ Applicstion.java
@@ -10,3 +10,30 @@ public class MultiplyTwoNumbers {
         System.out.println("The product is: " + product);
     }
 }
+#feature202 starts here
+public class Main {
+
+  public static void main(String args[]) {
+
+    String data = "This is the data in the output file";
+
+    try {
+      // Creates a FileWriter
+      FileWriter file = new FileWriter("output.txt");
+
+      // Creates a BufferedWriter
+      BufferedWriter output = new BufferedWriter(file);
+
+      // Writes the string to the file
+      output.write(data);
+
+      // Closes the writer
+      output.close();
+    }
+
+    catch (Exception e) {
+      e.getStackTrace();
+    }
+  }
+  #feature202 changes end
+}
